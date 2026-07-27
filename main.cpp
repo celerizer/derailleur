50e1fb23390ef6792cad850a7754c53ae931f5c4
celerizer <celerizer@gmail.com>
2026-07-26 22:35:21 -0500

    use xwayland hint on wayland

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 
 int main(int argc, char *argv[])
 {
+  /* Hint to use X11/XWayland on Linux for OpenGL context juggling */
+#if defined(Q_OS_UNIX) && !defined(Q_OS_MACOS)
+  if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM"))
+    qputenv("QT_QPA_PLATFORM", "xcb");
+#endif
+
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
