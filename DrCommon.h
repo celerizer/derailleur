cbf3e8278c74e71e79a830c286f4ade7054134c9
celerizer <celerizer@gmail.com>
2026-09-20 12:03:44 -0500

    Update DrCommon.h

--- DrCommon.h
+++ DrCommon.h
@@ -4,8 +4,8 @@
 #include <QFile>
 #include <QString>
 
-static const char *const DERAILLEUR_DATE_STRING = "September 6, 2026";
-static const char *const DERAILLEUR_RELEASE_STRING = "r10";
+static const char *const DERAILLEUR_DATE_STRING = "September 20, 2026";
+static const char *const DERAILLEUR_RELEASE_STRING = "r11";
 
 typedef enum
 {
